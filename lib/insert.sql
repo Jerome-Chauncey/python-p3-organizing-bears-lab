18d92bd00c456795ad692640fe901303117721bc
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-18 15:43:00 +0300

    Implements SQL QUERIES

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Black', 'Sanguine', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'Black', 'Choleric', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 20, 'F', 'Black', 'Melancholic', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 20, 'M', 'Black', 'Phlegmatic', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'Black', 'Easy-going', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'Black', 'Slow-to-warm', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 20, 'F', 'Black', 'Difficult', 'YES');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'M', 'Black', 'Highly Sensitive', 'YES');
